8c1dce81d03fbf2049010d33cea6507c8941d200
Ryan <ryanjoya@users.noreply.github.com>
2018-04-03 10:23:20 -0500

    Add files via upload

---
+++ Ide.java
@@ -0,0 +1,16 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a = 4, b = 22;
+		double c;
+		c = (double) (b / a) - b % a;// your code goes here
+	}
+}
